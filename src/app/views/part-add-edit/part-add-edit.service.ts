70590fbc49da2aba760685a665306f4c43ab65e1
ampledevelopers <55086705+ampledevelopers@users.noreply.github.com>
2026-06-11 18:09:51 +0530

    PATH part add edit

--- src/app/views/part-add-edit/part-add-edit.service.ts
+++ src/app/views/part-add-edit/part-add-edit.service.ts
@@ -8,14 +8,23 @@ import { map } from 'rxjs';
 })
 export class PartAddEditService {
   rootUrl = localStorage.getItem('rootUrl');
+  nestUrl = localStorage.getItem('nestUrl');
   reqHeader = new HttpHeaders({ 'Content-Type': 'application/x-www-form-urlencoded', 'No-Auth': 'True'});
+  private getHeaders(): HttpHeaders {
+    const token = localStorage.getItem('userToken');
+    return new HttpHeaders({
+      'Content-Type': 'application/x-www-form-urlencoded',
+      'No-Auth': 'True',
+      'x-api-key': token || ''
+    });
+  }
   constructor(private http: HttpClient) {
   }
 
   getPart(partid: string) {
-    const form = 'X_API_KEY=' + localStorage.getItem('userToken') + '&user_id=' + localStorage.getItem('userId') +
+    const form =  '&user_id=' + localStorage.getItem('userId') +
     '&part_no=' + partid;
-    return this.http.post(this.rootUrl + 'api/common/get_part_price_details', form, {headers : this.reqHeader});
+    return this.http.post(this.nestUrl + 'common/get_part_price_details', form, {headers : this.reqHeader});
   }
 
   getCityList() {
@@ -24,20 +33,20 @@ export class PartAddEditService {
   }
 
   addPart(reqdata: string) {
-    const form = 'X_API_KEY=' + localStorage.getItem('userToken') + '&user_id=' + localStorage.getItem('userId') +
+    const form =  '&user_id=' + localStorage.getItem('userId') +
     reqdata;
-    return this.http.post(this.rootUrl + 'api/common/save_part_price_details', form, {headers : this.reqHeader});
+    return this.http.post(this.nestUrl + 'common/save_part_price_details', form, {headers : this.getHeaders()});
   }
 
   updatePart(reqData: string) {
-    const form = 'X_API_KEY=' + localStorage.getItem('userToken') + '&user_id=' + localStorage.getItem('userId') +
+    const form =  '&user_id=' + localStorage.getItem('userId') +
     reqData;
-    return this.http.post(this.rootUrl + 'api/common/update_part_price_details', form, {headers : this.reqHeader});
+    return this.http.post(this.nestUrl + 'ticket_edit/update_part_price_details', form, {headers : this.getHeaders()});
   }
 
   addKbbPart(partNo: string, desc: string) {
-    const form = 'X_API_KEY=' + localStorage.getItem('userToken') + '&user_id=' + localStorage.getItem('userId') +
+    const form = '&user_id=' + localStorage.getItem('userId') +
     '&part_no=' + partNo + '&description=' + desc;
-    return this.http.post(this.rootUrl + 'api/common/save_whole_unit_part', form, {headers : this.reqHeader});
+    return this.http.post(this.nestUrl + 'common/save_whole_unit_part', form, {headers : this.getHeaders()});
   }
 }
