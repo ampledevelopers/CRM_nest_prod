b147fd8da857bd7b613a26594c86d65df43c044e
Avinash <avinash.ravichandran2@ample.co.in>
2026-07-07 10:37:57 +0530

    replenshment entry path change

--- src/app/views/kgb-inward/kgb-inward.service.ts
+++ src/app/views/kgb-inward/kgb-inward.service.ts
@@ -1,6 +1,5 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { HttpErrorResponse } from '@angular/common/http';
 import { HttpHeaders } from '@angular/common/http';
 
 @Injectable({
@@ -8,43 +7,58 @@ import { HttpHeaders } from '@angular/common/http';
 })
 export class KgbInwardService {
   rootUrl = localStorage.getItem('rootUrl');
+  nestUrl = localStorage.getItem('nestUrl');
   reqHeader = new HttpHeaders({ 'Content-Type': 'application/x-www-form-urlencoded', 'No-Auth': 'True'});
+
+  private getHeaders(): HttpHeaders {
+    const token = localStorage.getItem('userToken');
+    return new HttpHeaders({
+      'Content-Type': 'application/x-www-form-urlencoded',
+      'No-Auth': 'True',
+      'x-api-key': token || ''
+    });
+  }
 
   constructor(private http: HttpClient) {
   }
 
   getAwaitingSparesTickets() {
-    const form = 'X_API_KEY=' + localStorage.getItem('userToken') + '&user_id=' + localStorage.getItem('userId') ;
-    return this.http.post(this.rootUrl + 'api/tickets/awaiting_spares_tickets', form, {headers : this.reqHeader});
+    const form = 'user_id=' + localStorage.getItem('userId');
+    return this.http.post(this.nestUrl + 'shipment/awaiting_spares_tickets', form, { headers: this.getHeaders() });
   }
 
   getKgbDetails(tId: any, repair_partId: any) {
-    const form = 'X_API_KEY=' + localStorage.getItem('userToken') + '&user_id=' + localStorage.getItem('userId') +
-    '&ticket_id=' + tId + '&id=' + repair_partId ;
-    return this.http.post(this.rootUrl + 'api/tickets/kgb_details', form, {headers : this.reqHeader});
+    const form = 'user_id=' + localStorage.getItem('userId') +
+      '&ticket_id=' + tId + '&id=' + repair_partId;
+    return this.http.post(this.nestUrl + 'shipment/kgb_details', form, { headers: this.getHeaders() });
   }
 
   getAsnType(asn: any) {
-    const form = 'X_API_KEY=' + localStorage.getItem('userToken') + '&user_id=' + localStorage.getItem('userId') + '&asn_no=' + asn;
-        return this.http.get(this.rootUrl + 'api/ticketsv3/consignment_asn?' + form, {headers : this.reqHeader});
-      }
+    const form = 'user_id=' + localStorage.getItem('userId') + '&asn_no=' + asn;
+    return this.http.get(this.nestUrl + 'shipment/consignment_asn?' + form, { headers: this.getHeaders() });
+  }
+
+  // saveKgbDetails(kgbInput: any, kgbTicketId: any, repair_partid: any) {
+  //   const inputdata = JSON.stringify(kgbInput);
+  //   const form = 'user_id=' + localStorage.getItem('userId') +
+  //     '&kgbInput=' + inputdata + '&ticket_id=' + kgbTicketId + '&repair_partid=' + repair_partid;
+  //   return this.http.post(this.nestUrl + 'shipment/kgb_details_save', form, { headers: this.getHeaders() });
+  // }
 
   saveKgbDetails(kgbInput: any, kgbTicketId: any, repair_partid: any) {
-      const inputdata = JSON.stringify(kgbInput);
-    const form = 'X_API_KEY=' + localStorage.getItem('userToken') + '&user_id=' + localStorage.getItem('userId') +
-      '&kgbInput=' + inputdata + '&ticket_id=' + kgbTicketId  + '&repair_partid=' + repair_partid;
-      return this.http.post(this.rootUrl + 'api/tickets/kgb_details_save', form, {headers : this.reqHeader});
+    return this.http.post( this.nestUrl + 'shipment/kgb_details_save',
+      { user_id: localStorage.getItem('userId'), kgbInput: kgbInput, ticket_id: kgbTicketId, repair_partid: repair_partid, }, { headers: this.getHeaders() } );
   }
 
   saveConsignment(data: any) {
-    const form = 'X_API_KEY=' + localStorage.getItem('userToken') + '&user_id=' + localStorage.getItem('userId') + data;
-    return this.http.post(this.rootUrl + 'api/ticketsv3/kgbinward_save', form, {headers : this.reqHeader});
+    const form = 'user_id=' + localStorage.getItem('userId') + data;
+    return this.http.post(this.nestUrl + 'shipment/kgbinward_save', form, { headers: this.getHeaders() });
   }
 
   updateGsxRepair(tId: any, repairId: any) {
     const form = 'X_API_KEY=' + localStorage.getItem('userToken') + '&user_id=' + localStorage.getItem('userId') +
-    '&ticket_id=' + tId + '&repair_id=' + repairId ;
-    return this.http.post(this.rootUrl + 'api/tickets/update_gsx_repair', form, {headers : this.reqHeader});
+      '&ticket_id=' + tId + '&repair_id=' + repairId;
+    return this.http.post(this.rootUrl + 'api/tickets/update_gsx_repair', form, { headers: this.reqHeader });
   }
 
 }
